ecdee28930e8a22a51b9011fb89da5cc925cabf1
jetx1712 <87372329+jetx1712@users.noreply.github.com>
2024-04-30 18:18:06 -0300

    comit 30/04/2024

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Nova Energia</title>
     <!-- Inclua o link para o arquivo CSS do Bootstrap -->
     <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
@@ -16,7 +17,10 @@
 <body>
     <?php require ('header.php') ?>
     <section id="banner">
-        <img src="assets/images/banner.png" />
+        <div class="col-md-12">
+        <img class="col-md-12" src="assets/images/banner.png" />
+        </div>
+        
         <div class="texto-banner">
             <h1 class="titulo-banner">
                 Projeto<br>
@@ -74,7 +78,7 @@
                     <div>
                         <h2>Confira a linha do tempo do projeto</h2>
                         <label id="nova-energia">Nova Energia</label><br>
-                        <a class="saiba-mais" href="#"><i style="width: 30px;" class="fa-solid fa-arrow-right"></i> Ver
+                        <a class="saiba-mais" href="#"><i class="fa-solid fa-arrow-right"></i> Ver
                             agora</a>
                     </div>
                 </div>
@@ -98,21 +102,22 @@
                         <img src="assets/images/icons/whatsapp.png" />
                         <p style="margin-top: 6px" class="paragrafo-contato">WhatsApp</p>
                         <span class="titulo-contato">21 2000-1000</span>
-                        
+
                     </div>
                 </div>
                 <div class="col-md-4 blocos">
                     <div class="bloco">
                         <img src="assets/images/icons/mail.webp" />
                         <p style="margin-top: 6px" class="paragrafo-contato">Via email</p>
-                        <button><i style="width: 30px;" class="fa-solid fa-arrow-right"></i>Acesso ao formulário</button>
+                        <button><i style="width: 30px;" class="fa-solid fa-arrow-right"></i>Acesso ao
+                            formulário</button>
                     </div>
                 </div>
             </div>
         </div>
     </section>
     <footer>
-        <div style="background-color: #eaeaea;" class="row">
+        <div style="background-color: #eaeaea;" class="col-md-12">
             <div class="rodape">
                 <div>
                     <img src="assets/images/logo-rodape.png" />
@@ -122,18 +127,18 @@
                 </div>
             </div>
         </div>
-        <div style="justify-content:center" class="row">
-        <img style="max-width: 50%" src="assets/images/imagem-footer.png" />
-        <div>
-
-
-
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-            integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-            crossorigin="anonymous"></script>
+        <div style="justify-content:center" class="col-md-12">
+            <div class="logos-fundo">
+                <img src="assets/images/imagem-footer.png" />
+            </div>
+            <div>
+                <div>
+                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+                        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+                        crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+                        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+                        crossorigin="anonymous"></script>
     </footer>
 
 </body>
