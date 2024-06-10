08e7a5eb206ffdaed008d0094ca2addd296ca300
jetx1712 <87372329+jetx1712@users.noreply.github.com>
2024-06-10 17:50:03 -0300

    comit final

--- backend/ajax-edita-etapa-2.php
+++ backend/ajax-edita-etapa-2.php
@@ -2,12 +2,12 @@
 require('conexao.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["status_etapa_1"]) && isset($_POST["id_registro"])) {
+    if (isset($_POST["status_etapa_2"]) && isset($_POST["id_registro"])) {
         $id_registro = $_POST['id_registro'];
-        $status_etapa_1 = $_POST["status_etapa_1"];
+        $status_etapa_2 = $_POST["status_etapa_2"];
         
         // Atualizar o status na tabela etapas-1
-        $sql_update = "UPDATE `etapa-1` SET status_etapa = '$status_etapa_1', data = NOW() WHERE id_registro = $id_registro";
+        $sql_update = "UPDATE `etapa-2` SET status_etapa = '$status_etapa_2', data = NOW() WHERE id_registro = $id_registro";
         if ($conn->query($sql_update) === TRUE) {
             echo "<script>alert('O status da etapa foi atualizado com sucesso'); window.location.href = 'edicao.php';</script>";
         } else {
