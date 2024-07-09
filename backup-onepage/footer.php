6b5f2933db8ca1047d33c3dd15ff67276f50bbe6
jetx1712 <87372329+jetx1712@users.noreply.github.com>
2024-07-09 18:32:29 -0300

    comit final

---
+++ backup-onepage/footer.php
@@ -0,0 +1,11 @@
+<footer>
+        <img style="margin: 30px" src="images/logo-rodape.png" />
+        <p>Nova Energia - 2024 | Todos os direitos reservados</p>
+    </footer>
+    <section>
+        <div class="container">
+        <div class="imgfot">
+            <img src="images/imagem-footer.png" />
+        </div>
+        </div>
+    </section>
---
+++ backup-onepage/index.php
@@ -0,0 +1,121 @@
+<?php require_once ('header.php') ?>
+
+<body>
+    <section class="banner">
+        <div style="justify-content: center" class="row">
+            <img class="logo_top" src="images/logo-top.png" />
+        </div>
+        <div class="container">
+            <div class="bloco">
+                <h1>Conheça o material</h1>
+                <hr />
+                <p>Em breve, você vai poder acompanhar o andamento do projeto
+                    Nova Energia
+                    neste espaço. Por enquanto leia o material do projeto.
+                </p>
+                <a href="Workshop_Nova_Energia-Rev.1.8.pdf" target="a_blank"><button class="botao_bloco">Visualizar Workshop</button></a>
+                <img id="homi" src="images/homi.png" />
+
+            </div>
+        </div>
+        <img class="banner" src="images/banner.png" />
+        <div class="container">
+            <h1 class="titulo"> Projeto Nova Energia</h1>
+            <p class="subtitulo">O caminho para uma energia mais sustentável e com menos impacto para os prédios
+                públicos da região Metropolitana do Estado do Rio de Janeiro</p>
+        </div>
+
+    </section>
+
+    <section id="sobre">
+        <div class="container">
+            <h1 class="titulo_sobre">
+                Nova Energia, um projeto do instituto Rio Metrópole que traz soluções para a eficiência e a transição
+                energética
+                da Região Meropolitana do Rio de Janeiro
+            </h1>
+            <p style="margin-top: 30px">Como Alcançar a <span class="paragrafo_sobre">eficiência</span> e realizar a
+                <span class="paragrafo_sobre">transição energética</span> nos prédios públicos do <span
+                    class="paragrafo_sobre">Rio de Janeiro?</span>
+                Essas são algumas das muitas questões levantadas pelo <span class="paragrafo_sobre">Instituto Rio
+                    Metrópole (IRM)</span> quando o assunto envolve
+                a <span class="paragrafo_sobre">Região Metropolitana do Rio de Janeiro</span> Como seu compromisso é
+                buscar soluções tecnológicas de ponta
+                para a infraestrutura de transporte, conectividade, iluminação publica e modelos de eficiência
+                energética, o <span class="paragrafo_sobre">IRM</span>
+                lançou o projeto <span class="paragrafo_sobre">Nova Energia.</span>
+            </p>
+            <p style="margin-top: 30px">
+                A iniciativa vai criar um banco de dados e definir uma modelagem de transição, gestão e eficiência
+                energética em prédios
+                públicos de 19 municípios da Região Metropolitana - <span class="paragrafo_sobre">Belford Roxo,
+                    Cachoeiras de Macacu, Duque de Caxias, Guapimirim, Itaboraí, Japeri, Magé,
+                    Maricá, Mesquita,Nilópolis, Niterói, Paracambi, Petrópolis, Queimados, Rio Bonito, São Gonçalo,
+                    Seropédica e Tanguá </span> - e também os prédios públicos do Estado em todo seu território.
+                <span class="paragrafo_sobre"> Nova Iguaçu, São João de Meriti e Rio de Janeiro</span> já tem seus
+                programas próprios de eficiência energética.
+            </p>
+            <p>
+                O projeto procura reduzir as contas de energia, mapear características de consumo e identificar
+                oportunidades de
+                <span class="paragrafo_sobre">otimização financeira e eficiência energética.</span> Também prevê estudos
+                para transição de matriz energética
+                e estratégias de contratação de <span class="paragrafo_sobre">energia sustentável.</span>
+            </p>
+            <p>
+                Ele atende a dois dos 17 <span class="paragrafo_sobre">Objetivos de Desenvolvimento Sustentável
+                    (ODS)</span>, parte da chamada <span class="paragrafo_sobre">"Agenda 2030",</span> um pacto global
+                assinado durante a
+                <span class="paragrafo_sobre"> Cúpula das Nações Unidas</span> em 2015 pelos 193 países membros - o
+                Brasil é um deles. São elas:
+                <span class="paragrafo_sobre">ODS 7</span>(energia limnpa e acessível: garantir acesso á energia barata.
+                confiável, sustentável e renovável para todos) e <span class="paragrafo_sobre">ODS 13</span>(ação
+                contra a mudança global do clima: tomar medidas urgentes para combater a mudança climática e seus
+                impactos.).
+            </p>
+        </div>
+    </section>
+    <section id="beneficios">
+        <div class="container">
+            <h1 style="text-align:center;" class="titulo_beneficio">Os Benefícios</h1>
+            <div id="beneficios">
+                <div class="botão-beneficio">
+                    <div class="botao-dentro">
+                        <img src="images/icons/mao.png" />
+                        <p>Reduz gastos públicos, possibilitando direcionar o valor economizado para áreas como Saúde e
+                            Educação</p>
+                    </div>
+                </div>
+                <div class="botão-beneficio">
+                    <div class="botao-dentro">
+                        <img src="images/icons/mao-segurando-o-mundo.png" />
+                        <p style="margin-top: 5px;">Reduzir os impactos ambientais, contribuindo para que o brasil
+                            alcance as metas de
+                            descarbonização e <br> de controle do aquecimento global.</p>
+                    </div>
+                </div>
+                <div class="botão-beneficio">
+                    <div class="botao-dentro">
+                        <img src="images/icons/energia.png" />
+                        <p>Usar uma matriz energética mais sustentável, preferencialmente solar e biomassa.</p>
+                    </div>
+                </div>
+            </div>
+            <h3 style="text-align: center;">O projeto <strong>NOVA ENERGIA</strong> é financiado pelo IRM e pela <strong>Secretaria de
+                Planejamento e Gestão do Estado do Rio de Janeiro (SEPLAG)</strong></h3>
+            <h4 style="text-align: center; margin-bottom: 40px">Em breve, você vai poder acompanhar o adamento do
+                projeto Nova Energia neste espaço.</h4>
+        </div>
+    </section>
+    <?php require ('footer.php') ?>
+
+
+    <!-- Seu conteúdo aqui -->
+
+    <!-- jQuery (Necessário para os plugins JavaScript do Bootstrap) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
